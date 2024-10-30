47de7132fac7a85235eebd27e042d7e758d77491
Edwin-l-H <edwinlh2789@nycstudents.net>
2024-10-30 09:13:46 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,11 +5,26 @@ public class Main
         int[][] nums = {
             {12, 3, 4, 13, 5},
             {11, 21, 2, 14, 16},
-            {7, 8, 9, 25, 0},
+            {7, 8, 9, 15, 0},
             {10, 17, 20, 19, 1},
             {18, 22, 30, 25, 6}
         };
         GridPath one = new GridPath(nums);
-        System.out.println(one);
+        System.out.println(one.getNextLoc(0, 0));
+        System.out.println(one.getNextLoc(1, 3));
+        System.out.println(one.getNextLoc(2, 4));
+        System.out.println(one.getNextLoc(4, 3));
+
+
+        int [][] nums2 = {
+            {12, 30, 40, 25, 5},
+            {11, 3, 22, 15, 43},
+            {7, 2, 9, 4, 0},
+            {8, 33, 18, 6, 1}
+        };
+
+        GridPath two = new GridPath(nums2);
+        System.out.println(two.sumPath(1, 1));
+        System.out.println(two.sumPath(0, 4));
     }
 }

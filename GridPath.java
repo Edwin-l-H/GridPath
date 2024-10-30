c9451e547482281753b3d95b3ae2a2cb30db2d56
Edwin-l-H <edwinlh2789@nycstudents.net>
2024-10-30 09:13:58 -0400

    Update GridPath.java

--- GridPath.java
+++ GridPath.java
@@ -14,8 +14,24 @@ public class GridPath
      */
     public Location getNextLoc(int row, int col) 
     {
-        return null;
         /* to be implemented in part (a) */ 
+
+        //If the row requested is at the border, it would keep "below" at Integer.MAXVALUE there is no row below the bottom row.
+        //Keeping "below" at Integer.MAXVALUE will ensure that "right" gets returned if it exists.
+        int below = Integer.MAX_VALUE;
+        if (row < grid.length - 1) below = grid[row + 1][col];
+
+        int right = Integer.MAX_VALUE;
+        if (col < grid[0].length - 1) right = grid[row][col + 1];
+
+        if (below < right)
+        {
+            return new Location(row + 1, col);
+        }
+        else 
+        {
+            return new Location(row, col + 1);
+        }
     }
 
     /**
@@ -29,7 +45,18 @@ public class GridPath
      */
     public int sumPath(int row, int col) 
     {
-        return 0;
+        int sum = grid[row][col];
+
+        //This while statement ensures that the path stays within the grid
+        while (row < grid.length - 1 || col < grid[0].length - 1)
+        {
+            Location loc = getNextLoc(row, col);
+            row = loc.getRow();
+            col = loc.getCol();
+
+            sum += grid[row][col];
+        }
+        return sum;
         /* to be implemented in part (b) */ 
     }
 
@@ -52,4 +79,5 @@ public class GridPath
         return s;
     }
     
+
 }
